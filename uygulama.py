2a3ae9caf3db037668fe2142cf05bc7e1d8e4f80
Bugra Erdal <95551650+bugraerdal0@users.noreply.github.com>
2026-05-31 00:43:11 +0300

    Update uygulama.py

--- uygulama.py
+++ uygulama.py
@@ -9,7 +9,7 @@ st.markdown("""
 <style>
     /* Global font büyütme */
     html, body, [class*="css"] {
-        font-size: 28px !important;
+        font-size: 26px !important;
     }
     /* Metrik kutucukları */
     [data-testid="metric-container"] {
